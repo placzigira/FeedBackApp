959ad719c19ba49860ad2a00012b8d92de569511
placzigira <4thpzee@gmail.com>
2023-04-10 21:44:38 +0200

    Components Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Header from "./Components/Header";
 import FeedbackItem from "./Components/FeedbackItem";
+import Nav from "./Components/Navigation";
+import Corousel from "./Components/Corousel";
 const App = () => {
   return (
     <>
     <Header />
+    <Nav/>
     <div className="container">
     <FeedbackItem />
+    <Corousel/>
     </div>
     </>
   );
